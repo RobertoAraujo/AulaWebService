11c5cfa66e3f43add24da3120c6dafe06ebbd4f0
Roberto A Moraes <endriosrobert@gmail.com>
2024-05-14 20:48:25 -0500

    commit inicial

---
+++ src/main/java/io/github/robertoaraujo/rest/ApiErrors.java
@@ -0,0 +1,4 @@
+package io.github.robertoaraujo.rest;
+
+public class ApiErrors {
+}
---
+++ src/main/java/io/github/robertoaraujo/rest/controller/HelloController.java
@@ -0,0 +1,15 @@
+package io.github.robertoaraujo.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1")
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String index(){
+        return "Hello word!";
+    }
+}

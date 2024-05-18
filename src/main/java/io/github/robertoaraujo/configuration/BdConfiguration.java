717c40cc9dbc7cd59bccb61b5a3572ff0367330b
Roberto A Moraes <endriosrobert@gmail.com>
2024-05-17 20:06:47 -0500

    config de banco com properties

---
+++ src/main/java/io/github/robertoaraujo/configuration/BdConfiguration.java
@@ -0,0 +1,25 @@
+package io.github.robertoaraujo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application-mysql.properties")
+public class BdConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Bean
+    public String customPropertyBean() {
+        return "Datasource URL: " + datasourceUrl + ", Username: " + datasourceUsername;
+    }
+}
